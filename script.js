5eb0b8204e6d48b1cb6e3c511676b3ba1a09f13d
DarshanPro123 <darshanpro187@gmail.com>
2024-09-02 15:10:28 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// $(document).ready(function () {
+//     $('.slider-for').slick({
+//         dots: true,
+//         infinite: true,
+//         speed: 300,
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+//         arrows: true,
+//         adaptiveHeight: true
+//     });
+
+//     $('.action a').on('click', function (e) {
+//         e.preventDefault();
+//         var slideNo = $(this).data('slide');
+//         $('.slider-for').slick('slickGoTo', slideNo);
+//     });
+// });
